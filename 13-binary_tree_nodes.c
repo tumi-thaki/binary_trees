80fe17730cbf47acf9071681cc40558a882ddca1
Tumi Thaki <tumiethaki01@gmail.com>
2024-01-31 12:09:11 -0800

    13-binary_tree_nodes.c betty fixed

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,10 +13,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree)
 	{
 		if (tree->left || tree->right)
-			return 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-		else
-			 return (0);
+			return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return (0);
 	}
-	
 	return (0);
 }
