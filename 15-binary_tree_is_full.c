2b473527416d28d5eb5693113d52b190fed1dc7f
Tumi Thaki <tumiethaki01@gmail.com>
2024-02-01 10:00:30 -0800

    15-binary_tree_is_full.c betty fixed

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,9 +12,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		if ((tree->left != NULL && tree->right == NULL) || 
-				(tree->left == NULL && tree->right != NULL) || 
-				binary_tree_is_full(tree->left) == 0 || 
+		if ((tree->left != NULL && tree->right == NULL) ||
+				(tree->left == NULL && tree->right != NULL) ||
+				binary_tree_is_full(tree->left) == 0 ||
 				binary_tree_is_full(tree->right) == 0)
 			return (0);
 		return (1);
